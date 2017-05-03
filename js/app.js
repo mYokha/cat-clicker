7bd1e24e68c62e70e4e1ca183ebf4a722c0b573f
Max Yokha <max.yokha@gmail.com>
2017-05-04 02:33:09 +0300

    Make mods

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 var $score = $('#score');
-var $imageContainer = $('#cat-image');
 var $catImage = $('.cat');
 var score = 0;
 var imageSource = 'http://thecatapi.com/api/images/get?format=src&type=jpg';
+var $loadStatus = $('.loadStatus');
 
 $catImage.attr('src', imageSource);
-
-$imageContainer.click(function(e) {
-	$.ajax('http://thecatapi.com/api/images/get?format=src&type=jpg');
-	$catImage.attr('src', imageSource);
-	console.log('click!');
-	score++;
-	$score.html(score);
+/*
+var catRequestTimeout = setTimeout(function(){
+	    $catImage.attr(src, 'img/cat-placeholder.jpg');
+        $loadStatus.text('Image loading failed!<br>Click on the placeholder image to try to load an image again!');
+    }, 2000);
+*/
+$catImage.click(function() {
+	$.ajax({
+		url: imageSource,
+		success: function(){
+			$catImage.attr('src', imageSource);
+			score++;
+			$score.html(score);
+			//clearTimeout(catRequestTimeout);
+		},
+	}).fail(function() {
+		$catImage.attr('src', 'img/cat-placeholder.jpg');
+        $loadStatus.text('Image loading failed! Click on the placeholder image to try to load an image again!');
+	});
 });
